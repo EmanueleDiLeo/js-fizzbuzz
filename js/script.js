c679b362fd1c52c44a0180923450997048d2feb5
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-09-11 18:13:46 +0200

    [ADD] : else if

--- js/script.js
+++ js/script.js
@@ -5,28 +5,24 @@ for(let i=1; i<=100; i++){
   box.classList.add("box");
 
   if(!(i % 3) && !(i % 5)){
-    console.log("3 e 5 - i=", i, "FizzBuzz");
     box.classList.add("fizz-buzz");
     box.append("fizzbuzz");
     check= false;
   }
 
   else if(!(i % 3)){
-    console.log("3 - i=", i, i % 3 === 0,"Fizz");
     box.classList.add("fizz");
     box.append("fizz");
   }
 
   else if(!(i % 5)){
-    console.log("3 - i=", i, i % 5 === 0,"Buzz");
     box.classList.add("buzz");
     box.append("buzz");
   }
-  
+
   else{
     box.append(i);
   }
 
   bigBox.append(box);
-  console.log("-------------------------------------");
 }
